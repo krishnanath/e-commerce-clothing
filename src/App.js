1418ab9fc0e890c42ad42365d290709673e2246f
KRISHNANATH <krishnanath@KRISHNANATHs-MacBook-Air.local>
2019-09-30 12:09:59 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
  
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage =() => (
+  <div>
+    <h1>HATS PAGE</h1>
+    </div>
+);
+
+const TopicsList =() => (
+  <div>
+    <h1>Topics List</h1>
+    </div>
+);
+
+const TopicDetails = props => {
+  console.log(props);
+ return (
+  <div>
+    <h1>TOPICS DETAILS PAGE { props.match.params.topicId}</h1>
+    </div>
+);
+ };
 
 function App() {
   return (
     <div>
-  <HomePage />
+ <Route exact path='/' component={HomePage} />
+ <Route  path='/hats' component={HatsPage} />
+ <Route  path='/topics' component={TopicsList} />
+ <Route  path='/topics/:topicId' component={TopicDetails} />
+
+
 
     </div>
   );
